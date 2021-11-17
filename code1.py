7401b2fecbb4b00177d280f933bd4e24bd04081e
vietxb0911 <55185772+vietxb0911@users.noreply.github.com>
2021-11-18 01:11:40 +0700

    update code

--- code1.py
+++ code1.py
@@ -6,7 +6,7 @@ WORD_REGEX = re.compile(r"([a-zA-Z]+[-'][a-zA-Z]+)|([a-zA-z]+)")
 class WordCount(MRJob):
     def mapper(self, _, line): # ứng với bước map trong mapreduce
         for word in WORD_REGEX.findall(line): # tách câu thành các từ
-            yield(word, 1)
+            yield(word[1], 1)
     def reducer(self, word, count): # ứng với bước reduce trong mapreduce
         yield(word, sum(count))
 
